f87eebde10b41a6a81abe735ebacded482899d4f
Will.Lee <leaguematter@likelion.org>
2022-11-09 18:37:28 +0900

    frontend server

---
+++ components/agri-details/agri.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import AgriListDetail from './agri-list-detail';
+import AgriListBlackout from './agriListBlackout';
+import styles from '/styles/agri.module.css';
+
+const Agri = (props) => {
+
+    const agri = props.object;
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const modalChange = () => {
+        setModalVisible(!modalVisible);
+    };
+
+    return (
+        <>
+            <div className={styles['agri']} onClick={modalChange}>
+                <div className={styles['agri-frame']}>
+                    <div className={styles['picture-frame']}>
+                        <img className={styles['picture']} id="modalImage" src={agri.itemImage} />
+                    </div>
+                    <div className={styles['title']}>{agri.itemName}</div>
+                    <div className={styles['pot']}></div>
+                </div>
+            </div>
+            {modalVisible && <AgriListBlackout setIsModal={modalChange}/>}
+            {modalVisible && <AgriListDetail object={agri}/>}
+        </>
+    )
+}
+
+export default Agri
